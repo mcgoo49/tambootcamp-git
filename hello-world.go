5ab273901d24b7c6ca4af5398e19b67e63db10cb
Mark McGill <mmcgill@vmware.com>
2021-04-15 09:32:58 -0400

    added notes to hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -3,10 +3,10 @@
 # add a third comment
 # add a fourth comment
 # file:  hello-world.go
-# description:  my very first go program
+# description:  my very first go program - modified!
 
 package main
 import "fmt"
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("hello world modified")
 }
